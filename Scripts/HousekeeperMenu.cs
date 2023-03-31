d46330accd83b09ef2e1c365fd2d687410a63b52
zenas <zenas@quadratron.com>
2023-03-31 17:45:36 -0400

    Housekeeper Run - Persistent Playmode Scene Bug - added UNITY_EDITOR define - added Playmode change and Scene change hooks to set appropriate scene path to playModeStartScene

--- Scripts/HousekeeperMenu.cs
+++ Scripts/HousekeeperMenu.cs
@@ -1,13 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
+#if UNITY_EDITOR
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 
 namespace ZenTools.Housekeeper
 {
+    /// <summary>
+    /// Subscribes to the play mode state change event of the Unity Editor and
+    /// invoke the OnPlaymodeStateChange method.
+    /// </summary>
+    [InitializeOnLoad]
+    public static class PlaymodeStateHook
+    {
+        static PlaymodeStateHook()
+        {
+            EditorApplication.playModeStateChanged += HousekeeperMenu.OnPlaymodeStateChange;
+        }
+    }
+
+    /// <summary>
+    /// Subscribes to the active scene changed event of the Unity Editor
+    /// and invokes the OnSceneChange method.
+    /// </summary>
+    [InitializeOnLoad]
+    public static class SceneLoadHook
+    {
+        static SceneLoadHook()
+        {
+            EditorSceneManager.activeSceneChanged += HousekeeperMenu.OnSceneChange;
+        }
+    }
+
     public class HousekeeperMenu 
     {
+        private static string lastActiveScenePath = "";
+        private static bool isHousekeeperRun = false;
+
         /// <summary>
         /// Adds a new ZenTools/Housekeeper menu item called 'Run'.
         /// Gives the user the option to save the currently modified scenes.
@@ -16,20 +45,65 @@ namespace ZenTools.Housekeeper
         [MenuItem("ZenTools/Housekeeper/Run #&r")]
         private static void EnterPlaymodeWithScene0()
         {
-            try
+            if (EditorApplication.isPlaying)
             {
-                EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+                isHousekeeperRun = false;
+                EditorApplication.ExitPlaymode();
+            }
+            else
+            {
+                try
+                {
+                    // opens a dialog that asks the user if they want to save the current scene changes
+                    EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 
-                string scenePath = EditorBuildSettings.scenes[0].path;
+                    // get the current scene path and store it
+                    lastActiveScenePath = EditorSceneManager.GetActiveScene().path;
+
+                    // get the first scene in Build settings and set the Editor to use it when starting PlayMode
+                    string scenePath = EditorBuildSettings.scenes[0].path;
+                    EditorSceneManager.playModeStartScene = AssetDatabase.LoadAssetAtPath<SceneAsset>(scenePath);
+
+                    // if we are entering PlayMode via the Housekeeper Run menu item
+                    isHousekeeperRun = true;
+
+                    // enter PlayMode
+                    EditorApplication.EnterPlaymode();
+                    
+                }
+                catch
+                {
+                    Debug.LogError("Please add a scene to the Build Settings");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Called when the Editor PlayMode state changes.
+        /// isHousekeeperRun keeps tract if the current state
+        /// change was initiated by the Housekeeper menu item
+        /// </summary>
+        /// <param name="stateChange">the current PlayMode state</param>
+        public static void OnPlaymodeStateChange(PlayModeStateChange stateChange)
+        {
+            if (stateChange == PlayModeStateChange.EnteredPlayMode && !isHousekeeperRun)
+            {
+                string scenePath = lastActiveScenePath;
                 EditorSceneManager.playModeStartScene = AssetDatabase.LoadAssetAtPath<SceneAsset>(scenePath);
-
-                EditorApplication.EnterPlaymode();
             }
-            catch
-            {
-                Debug.LogError("Please add a scene to the Build Settings");
-            }
+        }
 
+        /// <summary>
+        /// Called when the Editor changes the active scene.
+        /// Sets the currently active scene to play when the
+        /// user uses the Play button to enter PlayMode.
+        /// </summary>
+        /// <param name="current">not used</param>
+        /// <param name="next">not used</param>
+        public static void OnSceneChange(Scene current, Scene next)
+        {
+            string scenePath = EditorSceneManager.GetActiveScene().path;
+            EditorSceneManager.playModeStartScene = AssetDatabase.LoadAssetAtPath<SceneAsset>(scenePath);
         }
 
         /// <summary>
@@ -44,3 +118,4 @@ namespace ZenTools.Housekeeper
         }
     }
 }
+#endif
